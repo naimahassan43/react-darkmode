71e2d9435c1a66c8217169cf6e9b9d8c45fd1408
Naima Hassan <naima.hassan434@gmail.com>
2022-06-05 21:12:30 +0100

    Moment js

--- src/Article.js
+++ src/Article.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import moment from 'moment'
-const Article = ({title, snippet,date,length}) => {
-  console.log(date)
-  return <article className="post">
-    <h2>{title}</h2>
-    <div className="post-info">
-      {/* <span>{date}</span> */}
-      <span>{length} min read</span>
-    </div>
-    <p>{snippet}</p>
-  </article>
-}
+import React from "react";
+import moment from "moment";
+const Article = ({ title, snippet, date, length }) => {
+  console.log(date);
+  return (
+    <article className="post">
+      <h2>{title}</h2>
+      <div className="post-info">
+        <span>{moment(date).format("MMM Do dddd , YYYY")}</span>
+        <span>{length} min read</span>
+      </div>
+      <p>{snippet}</p>
+    </article>
+  );
+};
 
-export default Article
+export default Article;
