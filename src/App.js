5e55a104c9cff2fdb3ec80d5eb3029d26435e2c0
Naima Hassan <naima.hassan434@gmail.com>
2022-06-05 21:27:49 +0100

    Toggle theme

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import Article from "./Article";
 
 function App() {
-  return <main>
-    <nav>
-      <div className="nav-center">
-        <h1>Overreacted</h1>
-        <button className="btn">toggle</button>
-      </div>
-    </nav>
-    <section className="articles">
-      {data.map((item)=>{
-        return <Article key={item.id} {...item}/>
-      })}
-    </section>
-  </main>
+  const [theme, setTheme] = useState("light-theme");
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>Overreacted</h1>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
